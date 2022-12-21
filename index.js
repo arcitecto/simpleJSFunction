8d6b0452728a1cdc3e47a986ea717d0d39f356f9
Lawrence Mitchell <94389382+arcitecto@users.noreply.github.com>
2022-12-21 17:51:45 -0600

    Create a basic JavaScript call and alert function that takes in 3 parameters and link it to an HTML file. (simple-js-function)

---
+++ index.js
@@ -0,0 +1,11 @@
+var name = 'Lawrence';
+var height = Number(5) + "'" + Number(11) + '"';
+var country = 'USA';
+
+function infoStats(name, height, country) {
+    console.log(name, height, country);
+    alert(name + " " + height + " " + country)
+}
+
+infoStats(name, height, country);
+
